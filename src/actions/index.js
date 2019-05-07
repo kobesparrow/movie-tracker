544afa67897985a3037e1ed079b0d7fe5a282900
kobesparrow <markcollins.md@gmail.com>
2019-05-07 16:53:43 -0600

    Fetch frustration

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,20 +2,7 @@ import { popularFetcher } from '../api/genreFetcher'
 
 export const displayPopularMovies = () => ({
   type: 'DISPLAY_POPULAR_MOVIES',
-  payload: popularFetcher()
+  payload: ['Duy', 'Mark']
 })
 
-export const fetchMoviesBegin = () => ({
-  type: 'FETCH_MOVIES_BEGIN'
-});
-
-export const fetchMoviesSucess = () => ({
-  type: 'FETCH_MOVIES_SUCCESS',
-  payload: { movieData }
-});
-
-export const fetchMoviesFailure = () => ({
-  type: 'FETCH_PRODUCTS_FAILURE',
-  payload: { error }
-})
 
