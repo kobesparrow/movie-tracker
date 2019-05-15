d1d34eda5b125bdf7b05a865f5e81da943a6373b
Duy Vu <duyv.thanh@gmail.com>
2019-05-14 22:26:44 -0600

    Test genreFetcher

--- src/api/genreFetcher.js
+++ src/api/genreFetcher.js
@@ -3,7 +3,6 @@ import { fetchAll } from './fetchAll'
 
 const popularFetcher = async (type) => {
   const url = 'https://api.themoviedb.org/3/movie/'
-  // const type = 'popular?';
   const api = `${apiKey}`
   try {
     const response = await fetchAll(`${url}${type}${api}&language=en-US&page=1`)
--- src/api/genreFetcher.test.js
+++ src/api/genreFetcher.test.js
@@ -1,9 +1,17 @@
 import {popularFetcher} from './genreFetcher';
+import { fetchAll } from './fetchAll'
+import { apiKey } from '../apikey'
 
 describe('popularFetcher', () => {
     let mockData;
+    let mockUrl; 
+    let mockType;
+    let mockApi;
 
     beforeEach(() => {
+        mockUrl = 'https://api.themoviedb.org/3/movie/'
+        mockApi = `${apiKey}`
+        mockType = 'popular?'
         mockData = {
             movieContainer: 'Popular',
             results: [
@@ -16,5 +24,11 @@ describe('popularFetcher', () => {
             ok: true,
             json: () => Promise.resolve(mockData)
         }))
+    });
+
+    it('should see if popularFetch is called with the right params', async () => {
+        popularFetcher(mockType)
+        await fetchAll(`${mockUrl}${mockType}${mockApi}&language=en-US&page=1`)
+        expect(fetch).toHaveBeenCalledWith(`${mockUrl}${mockType}${mockApi}&language=en-US&page=1`)
     });
 });
